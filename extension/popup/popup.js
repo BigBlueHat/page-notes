851f9f597dd3941cb05ca14ac5b5f47ce956b8f8
BigBlueHat <byoung@bigbluehat.com>
2016-11-11 15:54:51 -0500

    Initial commit; translatable page note annotations

---
+++ extension/popup/popup.js
@@ -0,0 +1,68 @@
+const translateApiKey = 'trnsl.1.1.20161110T161225Z.e921f6bf7d2941b2.2c92a0a9be39b02fba4ce72ba91afcdf69d0bb29';
+const translateURL = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${translateApiKey}`;
+/**
+ * Reference: https://tech.yandex.com/translate/doc/dg/reference/translate-docpage/
+ *
+ * key=<API key>
+   & text=<text to translate>
+   & lang=<translation direction>
+   & [format=<text format>]
+   & [options=<translation options>]
+   & [callback=<name of the callback function>]
+ **/
+
+
+$('.ui.checkbox').checkbox();
+
+var $form = $('#page-note');
+$form.on('submit', function(ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+  var formData = $form.form('get values');
+  var annotation = {
+    "@context": "http://www.w3.org/ns/anno.jsonld",
+    "type": "Annotation",
+    "body": {
+      "type": "Choice",
+      "items": [
+        {
+          "value": formData.value,
+          "language": "en"
+        }
+      ]
+    },
+    "target": location.href
+  };
+
+  // TODO: change arbitrary length text check to something smarter
+  if ('value' in formData && formData.value.length > 4) {
+    let languages = formData.language
+      .filter(function(lang) { return Boolean(lang); });
+
+    if (languages.length > 0) {
+      languages.forEach(function(lang) {
+        $.getJSON(`${translateURL}&text=${formData.value}&lang=en-${lang}`)
+          .then(function(resp) {
+            if ('code' in resp && resp.code === 200) {
+              annotation.body.items.push({
+                type: "TextualBody",
+                value: resp.text[0], // TODO: does it ever return more?
+                language: lang,
+                format: "text/plain",
+                creator: {
+                  id: "https://translate.yandex.net/api/v1.5/tr.json/translate",
+                  type: "Software",
+                  "schema:softwareVersion": "1.5",
+                  name: "Yandex Translate API",
+                  homepage: "https://tech.yandex.com/translate/"
+                }
+              });
+            }
+          });
+      });
+      console.log(annotation);
+    }
+  } else {
+    console.log(annotation);
+  }
+});
